85bca85800e96f6eac636e2844d1c351d95e5753
Sophie Blackwood <122307675+sophieBlackwood@users.noreply.github.com>
2026-04-23 15:37:57 -0600

    Update data.js

--- data.js
+++ data.js
@@ -130,7 +130,282 @@ That nothing makes sense to me
 `,
     tags: ["emotions", "Emotions Spinning"],
     featured: true
-  }
+  },
+  // More
+   {
+    id: 4,
+    title: "Flame",
+    date: "5/1/25",
+    content: `“It’s burning bright
+Throughout the night
+A dancing light
+The beautiful sight
+
+The flame as it sways
+Into the new day
+Sharing its rays
+And dancing in lovely ways
+
+Grow higher and stronger
+But let it stay longer
+It’s kindled and grows
+As it softly glows
+
+Be grateful for the warm 
+Don’t smother or swarm
+A heat that provides
+And doesn’t lie
+
+This flame is of friendship
+Of passion and of connection
+I’m thankful for my flame
+And for the person it frames”
+`,
+    tags: ["Flame"],
+    featured: true
+  },
+{
+    id: 5,
+    title: "Flaw",
+    date: "Unknown",
+    content: `“I know of one certain law
+That every single thing has a flaw
+
+Based on the facts it’s the only conclusion
+And can say anything else would be delusion
+
+Nobody is perfect and I’m no exception
+I live my life and this is my perception
+
+Wherever you look you can see light and dark
+Both are well known and both have their parts
+
+The smallest details are often most profound
+But also the things that can bring something down
+
+Despite the intention results can never be promised
+But bring us what can make us feel rawest
+
+We were never put to that impossible standard
+So why do we push ourselves to act in that manner
+
+A perfect circle can’t truly exist
+But still it appears when we look at it
+
+It’s ok to still hide them just be safe and ok
+But remember we all have them at the end of the day
+
+They can be reminders of so many things
+So what do you chose for them to bring
+
+I’m still not perfect even at accepting this message
+As I’m still a person not proud of these exceptions
+
+That’s another key to it even if small
+That it’s an exception not all
+
+Keep going, keep growing, keep all the scrawls
+And remember the lesson to be who you are”
+`,
+    tags: ["Flaw"],
+    featured: false
+  },
+{
+    id: 6,
+    title: "Flowers all around",
+    date: "4/2/25",
+    content: `“I see flowers all around, growing wherever there is room
+Petals soaking in sunshine, beautiful all in bloom
+
+
+Flowers being given in bouquets that smell so sweet
+But I get no flowers, it stings me all too deep
+
+
+They say love hurts and every rose has its thorns
+I know it’s true though it’s my flower that scorns
+
+
+My flowers all misshapen no beauty like the rest
+I’d keep my flower hidden I know that would be best
+
+
+But my flower grows and I give it another shot
+I still never listen, and it pricks me and it mocks
+
+
+I’ve received some flowers but they withered before I caught their gaze
+The flowers they delivered I never noticed in the haze
+
+
+I still love my flowers and wouldn’t change a thing
+But my flowers never help me, I still softly sing
+
+
+The beauty of the flowers yes it can truly suck
+I try to grow new flowers but I guess I’m out of luck
+
+
+Will my flowers call me when I’ve gone and moved away
+I want to see these flowers bloom another day
+
+
+Goodbye to my flowers though we know not for long
+For the joy the flowers bring me can never truly be wrong
+
+
+One day I’ll have my flowers though I know it won't be fast
+But I’ll never leave these flowers forgotten in the past” 
+`,
+    tags: ["Flowers All Around"],
+    featured: false
+  },
+{
+    id: 7,
+    title: "Foggy path",
+    date: "5/1/25",
+    content: `Walking down a foggy path
+I can’t see my way
+Not sure where I’m going
+But I’m going down it anyway
+
+The path still continues 
+It’s winding and long
+That I know for certain
+The other facts could be wrong
+
+The path could go so many ways
+There’s only one that I can take
+So I’ll go my path and try to be brave
+And remember to not be fake
+
+As I go down the path
+What’s behind grows more dim
+But new things become clear
+And I appreciate my win
+
+Memories may fail
+And fog may shroud
+But I’d still be there
+And push through that cloud
+
+A foggy path only I know
+Many others are like it
+And all have the same haze
+But each goes over a different pit
+
+The path is imposing with not knowing
+My journey still progressing
+It’s hard to go but that's part of the road
+I’ll continue changing and assessing 
+
+My light on this path
+It’s the same for all lovers
+It’s the bonds I have made
+And it’s the joy shared with others
+
+Thank you to my light
+That never will have a rift
+And thank you for the path
+This whole thing is a gift
+`,
+    tags: ["Foggy path"],
+    featured: false
+  },
+  {
+    id: 8,
+    title: "Frozen in Time",
+    date: "3/28/25",
+    content: `Frozen in time
+As people pass by
+My head is whirling
+But the world keeps turning
+I keep wishing
+But still everything’s missing
+Fighting an urge to cry
+I feel like I slowly die
+I’m not even sure as to why
+
+I feel I can’t speak
+I’m just too weak
+Would they even hear
+It’s all unclear
+Would they listen
+Or would that quicken
+The pace at which I’m forgotten
+
+I still can’t tell
+But I feel unwell
+I still don’t know
+Should I just go
+Stop please
+Listen and breathe
+Why did nobody see me
+
+`,
+    tags: ["Frozen in Time"],
+    featured: false
+  },
+  {
+    id: 9,
+    title: "Full system error ",
+    date: "Unknown",
+    content: `Brain is shutting down, senses are wrong 
+World is changing, what is going on?
+Why can’t I think, why am I not strong
+I just feel like saying, “I just need a pause”
+
+Body shaking
+Legs quaking
+Everything is off
+Look around and I see
+This only happened to me
+
+World keeps spinning 
+Friends still missing
+But nothing’s going on 
+
+I’m still breathing but no words escaping, 
+I'm just staying.
+ In a broken system that’s flawed 
+`,
+    tags: ["Full system error"],
+    featured: false
+  },
+  {
+    id: 10,
+    title: "Honest and Open",
+    date: "4/3/25",
+    content: `“Honest and open, is a friendship that is true 
+Honest and open, are words that connect me back to you 
+Honest and open, even on the worst of days 
+Honest and open, you help me through that haze 
+Honest and open, we know we both truly care 
+Honest and open, even if you’re not really there 
+Honest and open, with life and trials beware 
+Honest and open, you remind me to be hopeful and ignore the dread and swears 
+Honest and open, I know we always will 
+Honest and open, in moments that time stands still 
+Honest and open, I never could forget 
+Honest and open, you help me to be my best 
+Honest and open, for the rest of time 
+Honest and open, as we hear the clock bell chime 
+Honest and open, I'll always stay this fond  
+Honest and open, this truly unique bright bond 
+Honest and open, even if we never once more meet 
+Honest and open, those times that we both wept 
+Honest and open, kept in my heart so dear 
+Honest and open, I wish that you were here 
+Honest and open, these words are all about you 
+Honest and open, I hope you know that it’s true 
+Honest and open, I say it once again 
+Honest and open, I hope this friendship never ends 
+Honest and open, even if we never see 
+Honest and open, I still have you with me” 
+`,
+    tags: ["Honest and Open"],
+    featured: false
+  },
 ];
 
 // If you need to filter these for the "Featured" carousel on the home page:
