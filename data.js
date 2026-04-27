82dd5064b5cd630f6d0873760910fc57f675e1c1
Sophie Blackwood <122307675+sophieBlackwood@users.noreply.github.com>
2026-04-27 10:38:38 -0600

    Update data.js

--- data.js
+++ data.js
@@ -61,7 +61,7 @@ A cycle of life
 Though full of strife
 Will always be blessed to me.`,
     tags: ["Cycle of Life", "Cycle", "life", "hope", "existence"],
-    featured: true
+    featured: false
   },
   {
     id: 2,
@@ -125,8 +125,777 @@ I’m still breathing but no words escaping,
 I’m just staying. 
 In a broken system that’s flawed`,
     tags: ["full", "system", "Full System Error", "error", "overwhelmed", "stasis"],
+    featured: false
+  },
+  // More
+  {
+    id: 4,
+    title: "Contradiction",
+    date: "6/7/25",
+    description: "Story Behind Poem",
+    content: `Knowing what to do yet never knowing how to do it
+planning for the future but there’s always a part that won’t fit
+
+Wanting to spend more time with others but also needing space
+Then I feel alone when they’re gone and my heart starts to race
+
+Happy to be done but sad that it’s now gone
+I can’t tell how I feel and feeling everything all as one
+
+Having nothing to say and wanting to share it all
+I try to climb and I still fall
+
+Exhausted and yet still awake
+Memories and thoughts that I can’t shake
+
+Happy to remember and hurting all the same
+Doing good and still myself I blame
+
+A contradiction that’s also fact
+And so we know that life is that
+    `,
+    tags: ["Contradiction", "knowing", "hurting"],
+    featured: false
+  },
+
+   {
+    id: 5,
+    title: "Emotions Spinning",
+    date: "4/6/25",
+    description: "Story Behind Poem",
+    content: `Back and forth, up and down
+These are my emotions as my head spins around
+The time keeps ticking
+So I’ll keep wishing
+For something to make sense now
+I want you to see how
+It never seems
+That my thoughts even mean a thing
+
+My day gets brighter
+And my mood feels lighter
+I get to see
+People who mean so much to me
+But after all 
+I still fall
+And now I crash back down
+
+No clue what I should do
+I end up feeling all more blue
+I still don’t cry
+And that feels like a crime
+They ask about my day
+And I lie and say the same “ok”
+An auto response I rarely mean
+
+I still don’t know
+Where I’ll go
+I still will think
+And write with ink
+Never to decide
+And I confide
+That nothing makes sense to me
+    `,
+    tags: ["Emotions Spinning", "emotions", "spinning"],
+    featured: false
+  },
+  
+   {
+    id: 6,
+    title: "Flame",
+    date: "5/1/25",
+    description: "Story Behind Poem",
+    content: `It’s burning bright
+Throughout the night
+A dancing light
+The beautiful sight
+
+The flame as it sways
+Into the new day
+Sharing its rays
+And dancing in lovely ways
+
+Grow higher and stronger
+But let it stay longer
+It’s kindled and grows
+As it softly glows
+
+Be grateful for the warm 
+Don’t smother or swarm
+A heat that provides
+And doesn’t lie
+
+This flame is of friendship
+Of passion and of connection
+I’m thankful for my flame
+And for the person it frames
+    `,
+    tags: ["flame", "thankful", "grow", "friend"],
     featured: true
-  }
+  },
+
+   {
+    id: 7,
+    title: "Flaw",
+    date: "Unknown",
+    description: "Story Behind Poem",
+    content: `I know of one certain law
+That every single thing has a flaw
+
+Based on the facts it’s the only conclusion
+And can say anything else would be delusion
+
+Nobody is perfect and I’m no exception
+I live my life and this is my perception
+
+Wherever you look you can see light and dark
+Both are well known and both have their parts
+
+The smallest details are often most profound
+But also the things that can bring something down
+
+Despite the intention results can never be promised
+But bring us what can make us feel rawest
+
+We were never put to that impossible standard
+So why do we push ourselves to act in that manner
+
+A perfect circle can’t truly exist
+But still it appears when we look at it
+
+It’s ok to still hide them just be safe and ok
+But remember we all have them at the end of the day
+
+They can be reminders of so many things
+So what do you chose for them to bring
+
+I’m still not perfect even at accepting this message
+As I’m still a person not proud of these exceptions
+
+That’s another key to it even if small
+That it’s an exception not all
+
+Keep going, keep growing, keep all the scrawls
+And remember the lesson to be who you are
+    `,
+    tags: ["flaw", "growth", "change", "imperfection"],
+    featured: true
+  },
+
+   {
+    id: 8,
+    title: "Flowers all around",
+    date: "4/2/25",
+    description: "Story Behind Poem",
+    content: `I see flowers all around, growing wherever there is room
+Petals soaking in sunshine, beautiful all in bloom
+
+
+Flowers being given in bouquets that smell so sweet
+But I get no flowers, it stings me all too deep
+
+
+They say love hurts and every rose has its thorns
+I know it’s true though it’s my flower that scorns
+
+
+My flowers all misshapen no beauty like the rest
+I’d keep my flower hidden I know that would be best
+
+
+But my flower grows and I give it another shot
+I still never listen, and it pricks me and it mocks
+
+
+I’ve received some flowers but they withered before I caught their gaze
+The flowers they delivered I never noticed in the haze
+
+
+I still love my flowers and wouldn’t change a thing
+But my flowers never help me, I still softly sing
+
+
+The beauty of the flowers yes it can truly suck
+I try to grow new flowers but I guess I’m out of luck
+
+
+Will my flowers call me when I’ve gone and moved away
+I want to see these flowers bloom another day
+
+
+Goodbye to my flowers though we know not for long
+For the joy the flowers bring me can never truly be wrong
+
+
+One day I’ll have my flowers though I know it won't be fast
+But I’ll never leave these flowers forgotten in the past
+    `,
+    tags: ["flowers", "growth" "flowers all around", "change"],
+    featured: false
+  },
+
+   {
+    id: 9,
+    title: "Foggy Path",
+    date: "5/1/25",
+    description: "Story Behind Poem",
+    content: `Walking down a foggy path
+I can’t see my way
+Not sure where I’m going
+But I’m going down it anyway
+
+The path still continues 
+It’s winding and long
+That I know for certain
+The other facts could be wrong
+
+The path could go so many ways
+There’s only one that I can take
+So I’ll go my path and try to be brave
+And remember to not be fake
+
+As I go down the path
+What’s behind grows more dim
+But new things become clear
+And I appreciate my win
+
+Memories may fail
+And fog may shroud
+But I’d still be there
+And push through that cloud
+
+A foggy path only I know
+Many others are like it
+And all have the same haze
+But each goes over a different pit
+
+The path is imposing with not knowing
+My journey still progressing
+It’s hard to go but that's part of the road
+I’ll continue changing and assessing 
+
+My light on this path
+It’s the same for all lovers
+It’s the bonds I have made
+And it’s the joy shared with others
+
+Thank you to my light
+That never will have a rift
+And thank you for the path
+This whole thing is a gift
+    `,
+    tags: ["Foggy Path", "foggy", "growth", "journey"],
+    featured: false
+  },
+
+   {
+    id: 10,
+    title: "Frozen in Time",
+    date: "3/28/25",
+    description: "Story Behind Poem",
+    content: `Frozen in time
+As people pass by
+My head is whirling
+But the world keeps turning
+I keep wishing
+But still everything’s missing
+Fighting an urge to cry
+I feel like I slowly die
+I’m not even sure as to why
+
+I feel I can’t speak
+I’m just too weak
+Would they even hear
+It’s all unclear
+Would they listen
+Or would that quicken
+The pace at which I’m forgotten
+
+I still can’t tell
+But I feel unwell
+I still don’t know
+Should I just go
+Stop please
+Listen and breathe
+Why did nobody see me
+    `,
+    tags: ["Frozen in Time", "frozen", "sadness"],
+    featured: true
+  },
+
+   {
+    id: 11,
+    title: "Honest and Open",
+    date: "4/3/25",
+    description: "Story Behind Poem",
+    content: `Honest and open, is a friendship that is true 
+Honest and open, are words that connect me back to you 
+Honest and open, even on the worst of days 
+Honest and open, you help me through that haze 
+Honest and open, we know we both truly care 
+Honest and open, even if you’re not really there 
+Honest and open, with life and trials beware 
+Honest and open, you remind me to be hopeful and ignore the dread and swears 
+Honest and open, I know we always will 
+Honest and open, in moments that time stands still 
+Honest and open, I never could forget 
+Honest and open, you help me to be my best 
+Honest and open, for the rest of time 
+Honest and open, as we hear the clock bell chime 
+Honest and open, I'll always stay this fond  
+Honest and open, this truly unique bright bond 
+Honest and open, even if we never once more meet 
+Honest and open, those times that we both wept 
+Honest and open, kept in my heart so dear 
+Honest and open, I wish that you were here 
+Honest and open, these words are all about you 
+Honest and open, I hope you know that it’s true 
+Honest and open, I say it once again 
+Honest and open, I hope this friendship never ends 
+Honest and open, even if we never see 
+Honest and open, I still have you with me
+    `,
+    tags: ["Honest and Open", "friendship", "honest"],
+    featured: true
+  },
+
+   {
+    id: 12,
+    title: "Is It?",
+    date: "5/13/25",
+    description: "Story Behind Poem",
+    content: `Is it insanity
+To never truly know
+Is it crazy
+That I’ll still go
+
+Is it something
+To be here with me
+Is it nothing
+That I will leave
+
+Is it healthy
+To never truly cry
+Is it sickening 
+When I say goodbye
+
+Is it normal
+To try and deny
+Is it strange
+To always ask why
+
+Is it happy
+To go find a new way
+Is it sad
+To leave you in May
+
+Is it hello
+If I have to leave
+Is it goodbye
+If maybe we’ll still get to see
+
+Is it hope
+To see my future
+Is it gloom
+To be leaving here sooner
+
+Is it everything
+That I’m leaving here and now
+Is it nothing
+These last years of bows
+
+Is it precious 
+To share it all
+Is it worthless
+If I choke and fall
+
+Is it a question
+That even should be asked
+Is it a statement
+I’m making of the past
+
+Is it love
+The things that I say
+Is it hate
+If I go and move away
+
+Is it life
+To share and always send
+Is it death
+To have things come to an end
+
+Is it the cause
+Of time always ticking
+Is it the effect
+Of a life worth living
+    `,
+    tags: ["Is it", "questioning", "life"],
+    featured: false
+  },
+
+   {
+    id: 13,
+    title: "Luck",
+    date: "6/15/25",
+    description: "Story Behind Poem",
+    content: `I guess it’s time to roll the dice
+Every choice has a chance and price
+
+I have done so much
+But the truth is it’s often my luck
+
+I’m not as good as I may appear
+But it’s not something that others hear
+
+Every time another round
+And each one it all might come down 
+
+I don’t know if I could build it again
+Or just how different that it would be then
+
+What people would I still have
+And is that possible to stave
+
+I don’t know the difference of fate
+But I know I’m lucky at this rate
+
+Every minute is a gamble
+But I got through the preamble
+
+I’m glad that you’re my lucky charm
+And I’ll do my best to keep you from harm
+
+I’ll keep you safe by my side
+And hope our luck can turn the tide
+
+It’s nothing truly like a war
+But it’s not the same as it was before
+    `,
+    tags: ["luck", "fate", "change"],
+    featured: false
+  },
+
+   {
+    id: 14,
+    title: "Mistake",
+    date: "5/28/25",
+    description: "Story Behind Poem",
+    content: `I’ve made a mistake
+And I don’t want to again
+I’m afraid that I’ll break
+Though I don’t know when
+
+If I do the wrong thing
+What do they see
+If I go out and swing
+Is that good for me
+
+I don’t want to trip and stumble
+So I stop and I freeze
+And as the world turns to ruble
+I can only please
+
+I’d ask if that’s right
+To do it for others
+But I don’t want to blight
+So I stay through the summers
+
+I don’t go out of fear
+I don’t stay out of hope
+I get caught with a tear
+And I just try to cope
+
+I’ll try to learn
+And become something better
+And so I yearn
+Of the day that might be never
+
+I might keep slipping
+And falling to the ground
+I might keep missing
+The best part of the sound
+
+But I might do more
+Go farther than I can imagine
+Mistakes are a chore
+But maybe the best that can happen
+    `,
+    tags: ["mistake", "growth", "change"],
+    featured: false
+  },
+
+   {
+    id: 15,
+    title: "Moments in Memory",
+    date: "5/31/25",
+    description: "Story Behind Poem",
+    content: `“What?
+It could be many things
+Fact
+That binds us all like rings
+
+Me
+A person who you got to meet
+Us
+A group of people who formed bonds so sweet
+
+You
+A soul who was kind and fun
+We
+The individuals who all truly won
+
+When
+A time we didn’t know the other
+Then
+A moment we formed bonds in summer
+
+Now
+A time we experience things first hand
+Soon
+We realize it passes like wind blown sand
+
+Next
+A path we each get to take
+Beyond
+A time that gets left in the wake
+
+Where
+A place we all get to share
+There
+A picture of the best people anywhere
+
+Why
+A place to form connection
+Became
+Something stronger and it’s a gift from heaven
+
+How
+Is a question that doesn’t need to be asked
+Because
+Of a blessing and these things that will last
+    `,
+    tags: ["question", "moments in memory", "memory", "moment", "blessing"],
+    featured: false
+  },
+
+   {
+    id: 16,
+    title: "Symbol",
+    date: "5/7/25",
+    description: "Story Behind Poem",
+    content: `A symbol can be anything 
+A picture, feeling or sign
+They all represent something
+And each can change with time
+
+A shooting star cutting the sky
+A silent moment in the night
+A special reason to happily cry
+A special thing and source of light
+
+A squirrel that reminds me of moments
+Where we both confide
+A joke and a laugh filled with comments
+A time where there’s nothing to hide
+
+A class filled with people
+Learning how to act
+Where each and every person
+Is part of my life as a fact
+
+A drawing on a paper
+A joke that will last
+A photo on a camera
+A thing to hold from the past
+
+These all are symbols
+Ones that have meaning only to me
+That and the others
+Who are the ones who helped me see
+
+Thank you to those people
+For the symbols I hold dear
+And thank you to the reader
+For letting me be here
+    `,
+    tags: ["symbol", "friendship", "memory", "friend"],
+    featured: true
+  },
+
+   {
+    id: 17,
+    title: "Time Runs Out",
+    date: "Unknown",
+    description: "Story Behind Poem",
+    content: `Tick, here we go
+Tick, I already know
+Tick, look at the clock
+Tick, it’s moved a lot
+Tick, how much time?
+Tick, it passes by
+Tick, then once I need
+Tick, it freezes me
+Tick, once again
+Tick, it’s in my head
+Tick, an hour’s passed
+Tick, then a day too fast
+Tick, the week goes by
+Tick, and the month flies
+Tick, a year too much
+Tick, please don’t rush
+Tick, I want to slow
+Tick, I can’t control
+Tick, the speed of time
+Tick, a hollow chime
+Tick, I’ll make the most
+Tick, of the time I host
+    `,
+    tags: ["time runs out", "time", "rush", "tick"],
+    featured: false
+  },
+
+   {
+    id: 18,
+    title: "Unknown",
+    date: "5/22/25",
+    description: "Story Behind Poem",
+    content: `I’m wide awake past midnight
+As I’m writing here I sit
+Wrestling my thoughts I’ll fight
+But the truth still plainly hits
+
+I’m terrified of what’s coming
+They tell me that I’m free
+But patterns were so comforting 
+And now it’s up to me
+
+How do I know
+What I want or should go do
+It’s all just a show
+But I still have no clue
+
+I’m put out in the open
+Why is everyone so proud
+I feel like I’m broken
+I want to go back with the crowd
+
+Fears all stem from something
+It’s the one we all share
+It’s what we call unknowing
+And tell us to beware
+
+My head won’t go quite
+It’s dizzying and loud
+I’m blinded by this light
+In an ever looming cloud
+
+I’m loosing all my passion
+It’s gone and slipped away
+The world is looking ashen
+And it’s fading with the day
+
+Why is it so confusing
+When it makes sense to the rest
+And now I’m stuck here choosing
+I’m clearly far from the best
+
+So I’m wide awake past midnight
+As I’m writing here I sit
+Wrestling my thoughts I’ll fight
+But the truth still plainly hits
+    `,
+    tags: ["unknown", "confusion", "fear"],
+    featured: false
+  },
+
+   {
+    id: 19,
+    title: "Unravel",
+    date: "4/23/25",
+    description: "Story Behind Poem",
+    content: `I’m stuck in my unravel
+Being pulled along by my string
+No destination as I travel
+And I let the movements bring
+
+I say goodbye to those who knew me
+I’m not the person who I was
+I fall back into pieces
+Of the man who fell from above
+
+My friends all are precious 
+A treasure that I share
+I’ll never get to keep them
+As I’m damaged from the ware
+
+I’m not sure how I’m feeling
+I’ve been numbed to the rest
+I can’t say for certain
+As I’m failing to this test
+
+I fall apart completely
+And pretend I wasn’t there
+I try to shape the pieces
+And be the one who shares
+
+I can’t keep this going
+an act that went too fast
+My thread just snapped the needle
+and I’ve come apart at last
+    `,
+    tags: ["unravel", "fall apart", "numb", "change"],
+    featured: false
+  },
+  
+  {
+    id: 20,
+    title: "Wishes",
+    date: "6/9/25",
+    description: "Story Behind Poem",
+    content: `I’m making a wish
+For some many things
+Part is just hopes for this
+And part is a dream as it rings
+
+I wish I were something
+Someone talented and strong
+A person others would look to
+And they would see no wrong
+
+I wish I were better
+In so many different ways
+That I didn’t make mistakes and could do things over
+Or simply get out of this haze
+
+I wish I were more attractive
+Not for someone else
+But just that I can be self conscious
+A fact I know too well
+
+I wish I had more time
+To do the things I want
+Time to spend with others
+But it still would always haunt
+
+I wish I had an answer
+To every question I may ask
+A silent thought and wonder
+Or one that is a task
+
+I wish a thousand wishes
+And I wish a thousand more
+But the question of my wish is
+Who do I wish it for
+
+Is it good to wish for someone
+Is it selfish if it’s not
+Or is it good to be honest
+And be happy with what I got
+    `,
+    tags: ["wish", "hope", "change", "answers"],
+    featured: false
+  },
+  
 ];
 
 // Helper for the homepage logic
